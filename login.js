23c78d9555e7211edbd85430a38bfa16118a6f1a
Juicy <juicy@Juicys-MacBook-Pro.local>
2021-07-20 17:09:08 -0400

    added login, caching, and multi-page. ready for alpha

---
+++ login.js
@@ -0,0 +1,7 @@
+console.log('heeel.lo');
+var loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', function() {
+  console.log('clicked login');
+}, false);
+
+
--- popup.js
+++ popup.js
@@ -1,42 +1,120 @@
-console.log('testing123');
-
+// set up logic once DOM loads
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    
+  var bigDiv = document.getElementById('bigDiv');
+  var loginHTML = `<form id='loginForm'>
+                   <input type='email' name='email' id='email' placeholder='Email' autocomplete='username email'>
+	           <input type='password' name='password' id='password' placeholder='Password' autocomplete='new-password'>
+	           <button id='loginButton'>Sign In</button>
+	         </form>`;
+  var shareHTML = `<button id="addLink">add link</button>
+		  <p id='message'>So far, you've shared {{numLinks}} links</p>`;
+
+  function addLink(e) {  
     console.log("clicked button");
-
     chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      console.log("clicked button 2");
-      /*
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-      */
-      var f = d.createElement('form');
-      f.action = 'http://scriber.tech/loopd/add_link';
-      f.method = 'post';
-      var input0 = d.createElement('input');
-      input0.type = 'hidden';
-      input0.name = 'user_id';
-      input0.value = 1;
-      var input1 = d.createElement('input');
-      input1.type = 'hidden';
-      input1.name = 'link';
-      input1.value = tab.url;
-      f.appendChild(input0);
-      f.appendChild(input1);
-      d.body.appendChild(f);
-      f.submit();
+      var url = tab.url;
+      get_saved_user_cred()
+        .then(cred => {
+          const requestData = {email: cred.loopd_email, hash: cred.loopd_hash, link: url};
+	  fetch('http://scriber.tech/loopd/add_user_link', {
+	    method: 'POST',
+	    headers: {
+	      'Content-Type': 'application/json',
+	    },
+	    body: JSON.stringify(requestData),
+	  })
+          .then(res => {
+            if (res.status == 200) {
+	      console.log('LOOKIN GOOOOOOOD');
+	      
+	      document.getElementById('addLink').remove();
+	      document.getElementById('message').innerHTML = 'Link has been loopd!';
+	    } else {
+	      return Promise.reject('Failure on Loopd Server');
+	    }
+	  })
+	  .catch(err =>{ 
+	    console.log('bubbling up error: '+err);
+	  });
+	})
+	.catch(err => { console.log('add link failed: '+err);
+	});
     });
-  }, false);
-}, false);
+  }
+
+  function login(e) {
+    e.preventDefault();
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var salt = 'SquirrelPilotMango';
+    var hash = md5(salt+password);
+    const requestData = { email: email, hash: hash };
+    return make_login_request(requestData)
+	  .then(res => {
+	    change_page(shareHTML.replace(/{{numLinks}}/g, res.total_link_count));
+	    chrome.storage.local.remove('loopd_email');
+            chrome.storage.local.remove('loopd_hash');
+	    chrome.storage.local.set({loopd_email: email, loopd_hash: hash});
+	  })
+	  .catch(error => {console.log('Login failed: ', error);
+	  });
+  }
+   
+  function change_page(html) {
+    bigDiv.innerHTML = html;
+    var addLinkButton = document.getElementById('addLink');
+    var loginForm = document.getElementById('loginForm');
+    if (addLinkButton) {
+      addLinkButton.addEventListener('click', addLink);
+    } else if (loginButton) {
+      loginForm.addEventListener('submit', login);
+    }
+  }
+
+  function make_login_request(requestData) {
+    return new Promise((resolve, reject) => {
+      fetch('http://scriber.tech/loopd/login', {
+        method: 'POST',
+        headers: {
+	  'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+      })
+      .then(res => {
+	console.log('login response received');
+        if (res.status == 200) {
+          resolve(res.json());
+	} else {
+	  reject('Login Failed');
+        } 
+      })
+      .catch(error => { 
+        console.error('Error:', error); 
+        reject(error);
+      });
+    });
+  }
+
+  function get_saved_user_cred() {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(['loopd_email', 'loopd_hash'], function (response) {
+        if (chrome.runtime.lastError || response.loopd_email === undefined || response.loopd_hash === undefined)
+	  reject('no saved user credential');
+	resolve(response);
+      });
+    });
+  }
+
+  get_saved_user_cred()  
+    .then(cred => {
+      return make_login_request({email: cred.loopd_email, hash: cred.loopd_hash});
+    })
+    .then(res => {
+      change_page(shareHTML.replace(/{{numLinks}}/g, res.total_link_count));
+    })
+    .catch(err => {
+      console.log('Auto-login failed with: '+err);
+      change_page(loginHTML);
+    });
+});
+
